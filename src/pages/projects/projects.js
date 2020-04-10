dd205863e2df853d4a167bb051c418638506a833
Andrey Chebotar <andreyoneup@gmail.com>
2020-04-10 19:49:15 +0500

    Added timetable project, changed color of hero background

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -7,11 +7,12 @@ import Img from 'react-image';
 import school_thumbnail from '../../assets/project_thumbnail/1school_thumbnail2.png';
 import notesApp_thumbnail from '../../assets/project_thumbnail/notesApp_thumbnail2.png';
 import portfolio_thumbnail from '../../assets/project_thumbnail/portfolio_thumbnail2.png';
+import timetable_thumbnail from '../../assets/project_thumbnail/timetable_thumbnail2.png';
 
 import school_thumbnail_overlay from '../../assets/project_thumbnail/1school_thumbnail2-overlay.png';
 import notes_app_overlay from '../../assets/project_thumbnail/notesApp_thumbnail2-overlay.png';
 import portfolio_overlay from '../../assets/project_thumbnail/portfolio_thumbnail2-overlay.png';
-
+import timetable_overlay from '../../assets/project_thumbnail/timetable_thumbnail2-overlay.png';
 
 const Projects = (props) => {
     useEffect(() => {
@@ -24,48 +25,36 @@ const Projects = (props) => {
           <div className="page-content">
             <div className="content">
               <div className="projects-wrapper">
-                  <div className="project_item">
+
+                <div className="project_item">
                     <div className="project_item__img">
-                      <a href="https://1school-wptheme.000webhostapp.com/" target="_blank" rel="noopener noreferrer">
-                        
-                          <Img
-                            alt="Wordpress School theme"
-                            src={school_thumbnail}
-                            loader={
-                              <img
-                                src={school_thumbnail_overlay}
-                                alt="Wordpress School theme"
-                                className={'loader'}
-                                style={{
-                                  opacity: 1,
-                                  marginTop: "2px",
-                                  marginBottom: "2px"
-                                }}
-                              />
-                            }
-                          />
-                        
+                      <a href="https://github.com/AndreyCJ/College-Timetables" target="_blank" rel="noopener noreferrer">
+                        <Img
+                          src={timetable_thumbnail}
+                          alt="NotesApp"
+                          loader={
+                            <img src={timetable_overlay} alt="Wordpress School theme"/>
+                          }
+                        /> 
                       </a>
                     </div>
                     <div className="project_item__description">
-                        <h1>
-                          <a href="https://1school-wptheme.000webhostapp.com/" target="_blank" rel="noopener noreferrer">
-                            1shcool
-                          </a>
-                        </h1>
+                      <h1>
+                        <a href="https://github.com/AndreyCJ/College-Timetables" target="_blank" rel="noopener noreferrer">
+                          Расписание колледжа
+                        </a>
+                      </h1>
                       <p>
-                        Wordpress тема с поддержкой настраемового слайдера, постов, поиска по сайту.
+                        Прогрессивное web-приложение для просмотра расписания занятий, звонов, замен. 
                       </p>
                       <div className="project_item__links">
                         <div className="link">
-                          <a href="https://github.com/AndreyCJ/1school_WpTheme" target="_blank" rel="noopener noreferrer">GitHub</a>
-                        </div>
-                        <div className="link">
-                          <a href="https://1school-wptheme.000webhostapp.com/" target="_blank" rel="noopener noreferrer">Live Demo</a>
+                          <a href="https://github.com/AndreyCJ/College-Timetables" target="_blank" rel="noopener noreferrer">GitHub</a>
                         </div>
                       </div>
                     </div>
                   </div>
+                  
                   <div className="project_item">
                     <div className="project_item__img">
                       <a href="https://vanillajsnotesapp.000webhostapp.com/" target="_blank" rel="noopener noreferrer">
@@ -96,7 +85,7 @@ const Projects = (props) => {
                         </a>
                       </h1>
                       <p>
-                        Приложение для заметок, разработанное по паттерну проектирования AJAX на чистом JavaScript, PHP, MySQL.
+                        Web-приложение для заметок, разработанное по паттерну проектирования AJAX на чистом JavaScript, PHP, MySQL.
                       </p>
                       <div className="project_item__links">
                         <div className="link">
@@ -108,36 +97,50 @@ const Projects = (props) => {
                       </div>
                     </div>
                   </div>
-                {/* 
+
                   <div className="project_item">
                     <div className="project_item__img">
-                      <a href="https://vanillajsnotesapp.000webhostapp.com/" target="_blank" rel="noopener noreferrer">
-                        <Img
-                          src={notesApp_thumbnail}
-                          alt="NotesApp"
-                          loader={
-                            <img src={notes_app_overlay} alt="Wordpress School theme"/>
-                          }
-                        /> 
+                      <a href="https://1school-wptheme.000webhostapp.com/" target="_blank" rel="noopener noreferrer">
+                        
+                          <Img
+                            alt="Wordpress School theme"
+                            src={school_thumbnail}
+                            loader={
+                              <img
+                                src={school_thumbnail_overlay}
+                                alt="Wordpress School theme"
+                                className={'loader'}
+                                style={{
+                                  opacity: 1,
+                                  marginTop: "2px",
+                                  marginBottom: "2px"
+                                }}
+                              />
+                            }
+                          />
+                        
                       </a>
                     </div>
                     <div className="project_item__description">
-                      <h1>
-                        <a href="https://vanillajsnotesapp.000webhostapp.com/" target="_blank" rel="noopener noreferrer">
-                          Расписание колледжа
-                        </a>
-                      </h1>
+                        <h1>
+                          <a href="https://1school-wptheme.000webhostapp.com/" target="_blank" rel="noopener noreferrer">
+                            1shcool
+                          </a>
+                        </h1>
                       <p>
-                        Приложение для просмотра расписания занятий, звонов, замен в виде прогрессивного web-приложения. 
+                        Wordpress сайт с поддержкой настраемового слайдера, постов, поиска по сайту, виджетов.
                       </p>
                       <div className="project_item__links">
                         <div className="link">
-                          <a href="https://github.com/AndreyCJ/NotesApp" target="_blank" rel="noopener noreferrer">GitHub</a>
+                          <a href="https://github.com/AndreyCJ/1school_WpTheme" target="_blank" rel="noopener noreferrer">GitHub</a>
+                        </div>
+                        <div className="link">
+                          <a href="https://1school-wptheme.000webhostapp.com/" target="_blank" rel="noopener noreferrer">Live Demo</a>
                         </div>
                       </div>
                     </div>
                   </div>
-                 */}
+                
                 <div className="project_item">
                   <div className="project_item__img">
                     <a href="https://github.com/AndreyCJ/Portfolio" target="_blank" rel="noopener noreferrer">
