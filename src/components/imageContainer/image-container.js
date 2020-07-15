9e68cf031d2bac69575f183379742af01b2e80c9
Andrey Chebotar <andreyoneup@gmail.com>
2020-07-15 21:22:48 +0500

    for further work

--- src/components/imageContainer/image-container.js
+++ src/components/imageContainer/image-container.js
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
-import "./image-container.css";
+import React, { useState } from 'react';
+import './image-container.css';
+
+import useIntersectionObserver from '../../hooks/useIntersectionObserver';
 
 const ImageContainer = props => {
   const [imgLoaded, setImgLoaded] = useState(false);
+  const [setRef, visible] = useIntersectionObserver({ rootMargin: '200px' });
 
   return (
     <div 
       className={`image-container ${!imgLoaded && 'image-container--loading'}`}
       data-ratio={!imgLoaded && '16:9'}
+      
     >
-      <div>
-        <img
-          src={props.src}
-          alt={props.alt}
-          onLoad={() => setImgLoaded(true)}
-        />
-      </div>
+        <div ref={setRef}>
+          <img
+            
+            className="image-product"
+            src={visible || imgLoaded ? props.src : ''}
+            alt={props.alt}
+            onLoad={() => setImgLoaded(true)}
+            style={{display: `${!imgLoaded ? 'none' : 'block'}`}}
+          />
+        </div>
     </div>
   );
 };
